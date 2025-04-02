f39aba221e8819e53a54929b85377ebab0b5d2f6
Ondrej Sedlacek <xsedla1o@stud.fit.vutbr.cz>
2025-04-02 13:45:50 +0200

    fix(predictor): dtype for numpy array of lists

--- bert_pytorch/predict_log.py
+++ bert_pytorch/predict_log.py
@@ -137,8 +137,8 @@ class Predictor:
                 tim_seqs += tim_seq
 
         # sort seq_pairs by seq len
-        log_seqs = np.array(log_seqs)
-        tim_seqs = np.array(tim_seqs)
+        log_seqs = np.array(log_seqs, dtype=object)
+        tim_seqs = np.array(tim_seqs, dtype=object)
 
         test_len = list(map(len, log_seqs))
         test_sort_index = np.argsort(-1 * np.array(test_len))
